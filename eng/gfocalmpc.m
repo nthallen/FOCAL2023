c078543029322d97f597c03c4495aea6fb65481f
Norton Allen <allen@huarp.harvard.edu>
2023-06-13 13:58:34 -0400

    Eng files after ME,Alicat updates

---
+++ eng/gfocalmpc.m
@@ -0,0 +1,5 @@
+function fig = gfocalmpc(varargin);
+% gfocalmpc(...)
+% MPC
+ffig = ne_group(varargin,'MPC','pfocalmpct');
+if nargout > 0 fig = ffig; end
---
+++ eng/gfocalmpchk.m
@@ -0,0 +1,5 @@
+function fig = gfocalmpchk(varargin);
+% gfocalmpchk(...)
+% MPC HK
+ffig = ne_group(varargin,'MPC HK','pfocalmpchks','pfocalmpchkstale','pfocalmpchkt');
+if nargout > 0 fig = ffig; end
