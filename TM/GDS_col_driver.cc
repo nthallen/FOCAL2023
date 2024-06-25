58d53956b9d75e958186376d738d431fbe489ab6
Norton Allen <allen@huarp.harvard.edu>
2024-06-25 15:31:40 -0400

    Display GDS PTRH, include PTRH coeffs

--- TM/GDS_col_driver.cc
+++ TM/GDS_col_driver.cc
@@ -6,7 +6,8 @@ void GDS_col_driver::init() {
     msg(MSG, "%s: connected to %s", iname, get_subbus_name());
   }
   mr = pack_mread_request(n_reads_mr, "20:1:31,10,61:1:64,70");
-uint16_t coeffs[] = {0, 0, 0, 0, 0, 0};
+uint16_t coeffs[] =
+  {0xB2BB, 0xAD3D, 0x69EE, 0x6AFC, 0x7E3F, 0x67F2};
   check_ms8607_coeffs(0x65, coeffs);
 }
 
