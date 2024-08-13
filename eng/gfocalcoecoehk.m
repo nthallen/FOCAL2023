8d87db386300ebe79ca327a7ee9ef46ed7dd0f18
Norton Allen <allen@huarp.harvard.edu>
2024-08-13 11:41:05 -0400

    ADS1115 Status Bits

--- eng/gfocalcoecoehk.m
+++ eng/gfocalcoecoehk.m
@@ -1,5 +1,5 @@
 function fig = gfocalcoecoehk(varargin);
 % gfocalcoecoehk(...)
 % COE HK
-ffig = ne_group(varargin,'COE HK','pfocalcoecoehkaiic','pfocalcoecoehkn','pfocalcoecoehkc');
+ffig = ne_group(varargin,'COE HK','pfocalcoecoehkics','pfocalcoecoehkn','pfocalcoecoehkc');
 if nargout > 0 fig = ffig; end
--- eng/gfocalmoemoe.m
+++
@@ -1,5 +0,0 @@
-function fig = gfocalmoemoe(varargin);
-% gfocalmoemoe(...)
-% MOE
-ffig = ne_group(varargin,'MOE','pfocalmoemoecp','pfocalmoemoep','pfocalmoemoet','pfocalmoemoerh');
-if nargout > 0 fig = ffig; end
