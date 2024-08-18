29904c53a653480b588e08cd8cb5fe2426153c28
Norton Allen <allen@huarp.harvard.edu>
2024-08-18 11:08:29 -0400

    GDS_COE Coefficients

--- TM/GDS_COE_col_driver.cc
+++ TM/GDS_COE_col_driver.cc
@@ -4,6 +4,6 @@ void GDS_COE_col_driver::setup() {
   mr = pack_mread_request(n_reads_mr,
     "20,22:1:26,31,10,61:1:64,70");
   uint16_t coeffs[] =
-    { 0, 0, 0, 0, 0, 0};
+    { 0xA8EE, 0xAD60, 0x64EF, 0x697F, 0x7FF9, 0x6875};
   check_ms8607_coeffs(0x65, coeffs);
 }
